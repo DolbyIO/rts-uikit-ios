289f9d93b22956376b55e6ba690e7169014d6c6d
Mok, Casper <casper.mok@dolby.com>
2023-06-19 09:36:35 +1000

    Add tv OS 15

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "DolbyIORTSUIKit",
     defaultLocalization: "en",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v15), .tvOS(.v15)],
     products: [
         .library(
             name: "DolbyIOUIKit",
