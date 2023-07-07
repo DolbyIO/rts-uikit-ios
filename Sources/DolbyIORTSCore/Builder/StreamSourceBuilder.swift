e287b249fa62613b66d2debb0506a85886e9fb20
Ramesh Prasad <ramesh.prasad@dolby.com>
2023-07-07 14:34:18 +1000

    audio track

--- Sources/DolbyIORTSCore/Builder/StreamSourceBuilder.swift
+++ Sources/DolbyIORTSCore/Builder/StreamSourceBuilder.swift
@@ -111,9 +111,9 @@ final class StreamSourceBuilder {
     }
 
     func build() throws -> StreamSource {
-        guard !hasMissingAudioTrack else {
-            throw BuildError.missingAudioTrack
-        }
+//        guard !hasMissingAudioTrack else {
+//            throw BuildError.missingAudioTrack
+//        }
 
         guard !hasMissingVideoTrack, let videoTrack = videoTrack else {
             throw BuildError.missingVideoTrack
