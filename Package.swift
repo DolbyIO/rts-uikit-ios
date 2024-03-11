48e1914e110e1df741a5538d3e01e5bf7d2eb131
Aravind Raveendran <aravind.raveendran@dolby.com>
2024-03-11 12:11:45 +1100

    Add SDK drop 1.8.0

--- Package.swift
+++ Package.swift
@@ -18,9 +18,7 @@ let package = Package(
             name: "DolbyIORTSUIKit",
             targets: ["DolbyIORTSUIKit"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/millicast/millicast-sdk-swift-package", exact: "1.7.0")
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
@@ -33,9 +31,13 @@ let package = Package(
         .target(
             name: "DolbyIORTSCore",
             dependencies: [
-                .product(name: "MillicastSDK", package: "millicast-sdk-swift-package")
+                .byName(name: "MillicastSDK")
             ],
             path: "Sources/DolbyIORTSCore"
+        ),
+        .binaryTarget(
+            name: "MillicastSDK",
+            path: "LocalPackage/MillicastSDK.xcframework"
         ),
         .target(
             name: "DolbyIORTSUIKit",
